c86c95b8508dd4080ea1bed5accc7e6020720233
Michael Herman <hermanmu@gmail.com>
2015-04-07 07:20:16 -0600

    updated v2

--- tests/karma.conf.js
+++ tests/karma.conf.js
@@ -1,7 +1,13 @@
 module.exports = function(config) {
   config.set({
+
+    // base path that will be used to resolve all patterns
     basePath: '.',
+
+    // frameworks to use
     frameworks: ['mocha', 'chai'],
+
+    // list of files / patterns to load in the browser
     files: [
       '../app/bower_components/angular/angular.js',
       '../app/bower_components/jquery/dist/jquery.js',
@@ -12,12 +18,26 @@ module.exports = function(config) {
       './unit/*.js',
       '../app/app.js'
     ],
+
+    // test result reporter
     reporters: ['progress'],
+
+    // web server port
     port: 9876,
+
+    // enable / disable colors in the output (reporters and logs)
     colors: true,
+
+    // level of logging
     logLevel: config.LOG_INFO,
+
+    // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
+
+    // start these browsers
     browsers: ['Chrome'],
+
+    // Continuous Integration mode
     singleRun: false
   });
 };
